dc13a1b805c4d53e7ba53fb2dc92194871143e0c
Eric Lewis <ericlewis777@gmail.com>
2026-06-27 16:05:35 -0400

    Correct TUICraft upgrade cost model

--- src/server.ts
+++ src/server.ts
@@ -189,7 +189,7 @@ const DEFAULT_BOT_TUNING: BotTuningConfig = {
   earlyBossContactDistance: 1,
   maxWeaponUpgrade: 4,
   maxArmorUpgrade: 4,
-  upgradeCostBaseGold: 25,
+  upgradeCostBaseGold: 15,
   attackCooldownMs: 3_000
 };
 
